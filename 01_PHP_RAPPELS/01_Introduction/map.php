41429f07f6471db6b19b8b4a6065789d1576bf21
Nico <nicolas.desachy@gmail.com>
2022-06-13 14:09:11 +0200

    chap2

---
+++ 01_PHP_RAPPELS/01_Introduction/map.php
@@ -0,0 +1,19 @@
+
+<?php
+function cube($n)
+{
+    return ($n * $n * $n);
+}
+
+$a = [1, 2, 3, 4, 5];
+
+$b = array_map('cube', $a);
+
+
+foreach($a as &$n){
+    $n = cube($n);
+}
+
+print_r($b);
+
+
